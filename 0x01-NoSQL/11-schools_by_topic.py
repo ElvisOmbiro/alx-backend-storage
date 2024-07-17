7727d1c2eeed2fc02b6c6ee41caa2f95738a72c8
ElvisOmbiro <elvisombiro99@gmail.com>
2024-07-17 23:43:29 +0300

    11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed July  17 23:47:00 2024
+
+@Author: Nicanor Kyamba
+"""
+
+
+def schools_by_topic(mongo_collection, topic: str):
+    """
+    Returns a list of schools that have a certain topic.
+
+    Args:
+        mongo_collection (Collection): A pymongo collection.
+        topic (str): The topic to search for.
+
+    Returns:
+        List[str]: A list of schools that have a certain topic.
+    """
+    return list(mongo_collection.find({"topics": topic}))
